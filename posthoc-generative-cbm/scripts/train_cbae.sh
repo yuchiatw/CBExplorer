92da23f0fbf1c36d493a9f647f2d2ccd9377c138
jz2003 <zhaojianxing2003@gmail.com>
2025-12-01 03:28:37 -0800

    delete the cbm dir outside

--- posthoc-generative-cbm/scripts/train_cbae.sh
+++
@@ -1,7 +0,0 @@
-
-
-### training CB-AE for CelebA-HQ-pretrained StyleGAN2 with supervised models as pseudolabeler
-# python3 -u train/train_cbae_gan.py -e cbae_stygan2_thr90 -p supervised -t sup_pl_cls8
-
-### training CC for CelebA-HQ-pretrained StyleGAN2 with supervised models as pseudolabeler
-python3 -u train/train_cc_gan.py -e cc_stygan2_thr90 -p supervised -t sup_pl_cls8
