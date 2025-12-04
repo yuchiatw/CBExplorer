4a3658d41cc5da1bf7e4bc10e7f4ca0cc4ff9cab
Yu-Chia Huang <claire.hsinchu@gmail.com>
2025-12-03 22:36:05 -0800

    barchart

---
+++ frontend/src/components/barchart.jsx
@@ -0,0 +1,65 @@
+import * as d3 from 'd3';
+import { useEffect, useRef } from 'react';
+
+const margin = { top: 20, right: 30, bottom: 40, left: 40 };
+const data = [0.5, 0.6, 0.8, 0.4, 0.9, 0.7, 0.3, 0.2, 1.0, 0.55];
+
+export default function BarChart() {
+    const containerRef = useRef(null);
+    const svgRef = useRef(null);
+    useEffect(() => {
+
+        if (!svgRef.current || !containerRef.current) {
+            return;
+        }
+        const { width, height } = containerRef.current.getBoundingClientRect();
+        if (width && height) {
+            renderChart(svgRef.current, width, height);
+        }
+    }, []);
+    return (
+        <div ref={containerRef} style={{ width: "400px", height: "400px" }}>
+            <svg ref={svgRef} width={400} height={400}></svg>
+        </div>
+    );
+}
+
+function renderChart(svgElement, width, height) {
+    const svg = d3.select(svgElement);
+    svg.selectAll('*').remove();
+
+    const yIndex = d3.range(data.length);
+    const yScale = d3.scaleBand()
+        .rangeRound([margin.top, height - margin.bottom])
+        .domain(yIndex)
+        .padding(0.1);
+
+    const xScale = d3.scaleLinear()
+        .rangeRound([margin.left, width - margin.right])
+        .domain([0, 1]);
+
+    svg.append('g')
+        .attr('transform', `translate(${margin.left},0)`)
+        .call(d3.axisLeft(yScale).tickFormat(i => `Item ${i + 1}`).tickSizeOuter(0));
+
+    svg.append('g')
+        .attr('transform', `translate(0,${margin.top})`)
+        .call(d3.axisTop(xScale).ticks(10, '%'));
+
+    const eachrow = svg.selectAll('.bar')
+        .data(data)
+    eachrow
+        .join('rect')
+        .attr('x', xScale(0))
+        .attr('y', (d, i) => yScale(i))
+        .attr('width', (d) => xScale(d) - margin.left)
+        .attr('height', yScale.bandwidth())
+        .attr('fill', 'steelblue');
+    eachrow
+        .join('rect')
+        .attr('x', d => xScale(d))
+        .attr('y', (d, i) => yScale(i))
+        .attr('width', d => xScale(1 - d) - margin.left)
+        .attr('height', yScale.bandwidth())
+        .attr('fill', 'pink');
+}   
